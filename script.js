fe1a46f36d9e7c84174d872b93d71969bb0db230
Alex Santos de Oliveira <119976670+alex-oliveira1@users.noreply.github.com>
2023-01-12 16:18:35 -0300

    reconhece html
    Reconhecer html na caixa de texto

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var entrada = document.getElementById("entrada");
 var botaoEnviar = document.getElementById("enviar");
 var saida = document.getElementById("saida");
 function copiarEntrada(){
-    saida.textContent = entrada.value;
+    saida.innerHTML = entrada.value;
 }
 
 botaoEnviar.addEventListener("click", copiarEntrada);
